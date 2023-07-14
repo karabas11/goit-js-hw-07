887506a7ebf315b45ffcb72b3d0f1afa285f4d0b
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-07-14 12:43:19 +0300

    qu 1,2 no <Esc>
    qu 1,2 no <Esc>

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallerySmallImg = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      loading="lazy"
+      alt="${description}"
+     
+    />
+  </a>
+</li>`,
+  )
+  .join('');
+
+gallerySmallImg.insertAdjacentHTML('beforeend', markup);
+
+gallerySmallImg.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+
+  const bigImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${bigImg}"></img>`);
+
+  instance.show();
+}
